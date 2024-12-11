f7c82682e895b9c54b5c02e81abaafa205c84899
elyessassi <mjoxen@gmail.com>
2024-12-11 15:41:24 +0100

    main component

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='App-header' alt="holberton logo">
+        <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body' alt="holberton logo"><p>Login to access the full dashboard</p></div>
+      <div className='App-footer' alt="holberton logo"><p>Copyright 2024 - holberton School</p></div>
+    </>
+  )
+}
+
+export default App
