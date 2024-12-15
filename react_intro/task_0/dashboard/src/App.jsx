10cd648ac8cc12bb4913ab9f0cbf8c9141417b35
elyessassi <mjoxen@gmail.com>
2024-12-15 21:36:15 +0100

    adding the Notif component as a chlid component

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,15 +1,18 @@
 
 import './App.css'
+import {getCurrentYear, getFooterCopy} from "./utils.js" 
+import Notif from './Notifications.jsx'
 
 function App() {
   return (
     <>
+    <div className='root-notifications'><Notif /></div>
       <div className='App-header' alt="holberton logo">
         <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
-        <h1>School dashboard</h1>
+        <h1 role="heading">School dashboard</h1>
       </div>
-      <div className='App-body' alt="holberton logo"><p>Login to access the full dashboard</p></div>
-      <div className='App-footer' alt="holberton logo"><p>Copyright 2024 - holberton School</p></div>
+      <div className='App-body'><p>Login to access the full dashboard</p></div>
+      <div className='App-footer'><p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p></div>
     </>
   )
 }
