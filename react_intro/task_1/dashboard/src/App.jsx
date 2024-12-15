10e15b39edcff367d53192062c9c3e508a2b2ffb
elyessassi <mjoxen@gmail.com>
2024-12-15 22:15:24 +0100

    App component

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import {getCurrentYear, getFooterCopy} from "./utils.js" 
+import Notif from './Notifications.jsx'
+
+function App() {
+  return (
+    <>
+    <div className='root-notifications'><Notif /></div>
+      <div className='App-header' alt="holberton logo">
+        <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
+        <h1 role="heading">School dashboard</h1>
+      </div>
+      <div className='App-body'><p>Login to access the full dashboard</p></div>
+      <div className='App-footer'><p>Copyright {getCurrentYear()} - {getFooterCopy()}</p></div>
+    </>
+  )
+}
+
+export default App
