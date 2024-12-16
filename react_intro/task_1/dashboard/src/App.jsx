463b55b274201efdc8d883d7a9ef2401d888425d
elyessassi <mjoxen@gmail.com>
2024-12-16 10:33:05 +0100

    changing the name of Notif component to Notifications

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
 import {getCurrentYear, getFooterCopy} from "./utils.js" 
-import Notif from './Notifications.jsx'
+import Notifications from './Notifications.jsx'
 
 function App() {
   return (
     <>
-    <div className='root-notifications'><Notif /></div>
+    <div className='root-notifications'><Notifications /></div>
       <div className='App-header' alt="holberton logo">
         <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
         <h1 role="heading">School dashboard</h1>
