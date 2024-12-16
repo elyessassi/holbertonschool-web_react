99d374a711a04f517c379fc1a6c8c4bcf69b1c83
elyessassi <mjoxen@gmail.com>
2024-12-16 10:37:48 +0100

    returning a div that containes a paragraph instead of a div only

--- react_intro/task_1/dashboard/src/Notifications.jsx
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -1,7 +1,7 @@
 import "./Notifications.css"
 
 function Notifications() {
-    return (<div className="notifications">Here is the list of notifications</div>)
+    return (<div className="notifications"><p>Here is the list of notifications</p></div>)
 }
 
 export default Notifications
