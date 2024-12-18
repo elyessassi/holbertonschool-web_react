69a375ea8f5b4f9f8e7c075df2106464385152f7
elyessassi <mjoxen@gmail.com>
2024-12-18 20:56:35 +0100

    adding getLatestNotification function

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -6,3 +6,7 @@ export function getCurrentYear() {
 export function getFooterCopy(isIndex) {
     return isIndex ? "Holberton School" : "Holberton School main dashboard"
 }
+
+export function getLatestNotification() {
+    return ("<strong>Urgent requirement</strong> - complete by EOD")
+}
