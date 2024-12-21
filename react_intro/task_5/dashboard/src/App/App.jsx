6507efa7ae84ca196ae9eeb1c45f512df2a3b884
elyessassi <mjoxen@gmail.com>
2024-12-22 00:14:22 +0100

    restructuring the project

---
+++ react_intro/task_5/dashboard/src/App/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import {getCurrentYear, getFooterCopy} from "../utils/utils.js" 
+import Notifications from '../Notifications/Notifications.jsx'
+
+function App() {
+  return (
+    <>
+    <div className='root-notifications'><Notifications /></div>
+      <div className='App-header' alt="holberton logo">
+        <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
+        <h1 role="heading">School dashboard</h1>
+      </div>
+      <div className='App-body'><p>Login to access the full dashboard</p></div>
+      <label htmlFor="email">Email</label>
+      <input id="email" data-testid="email"></input>
+      <label htmlFor="pw">Password</label>
+      <input id="pw" data-testid="pw"></input>
+      <button data-testid="btn" type='submit'>OK</button>
+      <div className='App-footer'><p>Copyright {getCurrentYear()} - {getFooterCopy()}</p></div>
+    </>
+  )
+}
+
+export default App
