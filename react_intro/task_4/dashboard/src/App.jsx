7b8dc68f0984f1ed173812cc9467af800677c851
elyessassi <mjoxen@gmail.com>
2024-12-22 00:03:56 +0100

    adding task_4 directory for deployment

--- react_intro/task_4/dashboard/src/App.jsx
+++ react_intro/task_4/dashboard/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <div className='root-notifications'><Notifications /></div>
       <div className='App-header' alt="holberton logo">
-        <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
+        <img src="/assets/holberton-logo.jpg" alt="holberton logo" ></img>
         <h1 role="heading">School dashboard</h1>
       </div>
       <div className='App-body'><p>Login to access the full dashboard</p>
