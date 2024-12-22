726d65b5411d4c37cb104bcbe7523af69a80d906
elyessassi <mjoxen@gmail.com>
2024-12-22 21:16:03 +0100

    correcting the way to test labels

--- react_props/task_1/dashboard/src/Header/Header.spec.js
+++ react_props/task_1/dashboard/src/Header/Header.spec.js
@@ -12,4 +12,9 @@ describe("Header component", () => {
         const img = screen.getByAltText("holberton logo")
         expect(img).toBeInTheDocument()
     })
+    it("checking if the h1 is with the correct text", () => {
+        render(<Header />)
+        const h1elem = screen.getByRole("heading")
+        expect(h1elem.textContent).toBe("School dashboard")
+    })
 })
