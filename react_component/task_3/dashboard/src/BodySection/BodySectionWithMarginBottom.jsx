a7c8fe93789ed00fbe24fcbec3f4ffbb752309c9
elyessassi <mjoxen@gmail.com>
2025-01-30 22:34:48 +0100

    adding a BodySectionWithMarginBottom component

---
+++ react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -0,0 +1,5 @@
+import BodySection from './BodySection'
+
+export default function BodySectionWithMarginBottom(props) {
+    return (<><div className="bodySectionWithMargin" role='bswm'><BodySection props={props}></BodySection></div></>)
+}
