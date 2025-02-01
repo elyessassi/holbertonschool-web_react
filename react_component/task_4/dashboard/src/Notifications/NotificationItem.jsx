d18d0a8b936e74e83fc786aa9f7d53f1736fd370
elyessassi <mjoxen@gmail.com>
2025-02-02 00:39:48 +0100

    making NotificationList component a pure component

--- react_component/task_4/dashboard/src/Notifications/NotificationItem.jsx
+++ react_component/task_4/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types"
 import "./Notifications.css"
 import {v4 as uuid4} from "uuid"
-import { Component } from "react"
+import { Component ,PureComponent } from "react"
 
-class NotificationItem extends Component {
+class NotificationItem extends PureComponent {
     render() {
         if (this.props.html != undefined) {
             let id = uuid4()
