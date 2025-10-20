562821cf6146ed173002f3e2f4526ed8ea467c18
elyessassi <mjoxen@gmail.com>
2025-10-20 14:53:32 +0100

    adding correct color for text

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <h1 role="heading">School dashboard</h1>
       </div>
       <div className='App-body'><p>Login to access the full dashboard</p></div>
-      <div className='App-footer'><p>Copyright 2024 - holberton School</p></div>
+      <div className='App-footer'><p>Copyright 2025 - holberton School</p></div>
     </>
   )
 }
