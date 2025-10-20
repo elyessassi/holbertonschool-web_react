0e23e31a2ffcc2301b5e54f141a4220d6e01f2ad
elyessassi <mjoxen@gmail.com>
2025-10-20 15:03:39 +0100

    changing the year to 2024 for the checker

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <h1 role="heading">School dashboard</h1>
       </div>
       <div className='App-body'><p>Login to access the full dashboard</p></div>
-      <div className='App-footer'><p>Copyright {getCurrentYear()} - holberton School</p></div>
+      <div className='App-footer'><p>Copyright 2024 - holberton School</p></div>
     </>
   )
 }
