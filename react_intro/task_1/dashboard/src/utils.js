a29e7e5fce3f6fce5669b3ee231353237d963a2c
elyessassi <mjoxen@gmail.com>
2025-10-20 15:00:13 +0100

    removing unnecessary footer function

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -3,6 +3,3 @@ export function getCurrentYear() {
     return dataobj.getFullYear()
 }
 
-export function getFooterCopy(isIndex) {
-    return isIndex ? "Holberton School" : "Holberton School main dashboard"
-}
