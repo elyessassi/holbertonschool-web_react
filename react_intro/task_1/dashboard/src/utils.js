48bc04a1fb65a6724507384fd72a09227872ef7f
elyessassi <mjoxen@gmail.com>
2025-11-16 23:10:44 +0100

    adding getFooterCopy back

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -3,3 +3,6 @@ export function getCurrentYear() {
     return dataobj.getFullYear()
 }
 
+export function getFooterCopy(isIndex) {
+    return isIndex ? "Holberton School" : "Holberton School main dashboard"
+}
